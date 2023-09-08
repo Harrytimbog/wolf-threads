5f042caa2096e422e6454c4736595a4e0510725c
Timilehin Arigbede <harriemannie@gmail.com>
2023-09-08 11:09:12 +0100

    Load Admin User

---
+++ scripts/load-admin-user.ts
@@ -0,0 +1,21 @@
+import { getClient } from "@/connnectDB";
+import bcrypt from "bcrypt";
+
+async function loadAdminUser(username: string, password: string) {
+  console.log(`executing loading admin user ${username} pw ${password}`);
+  const saltRounds = 10;
+  const hashedPassword = await bcrypt.hash(password, saltRounds);
+  const client = await getClient();
+  await client.connect();
+  await client.query(
+    "insert into public.users (username, password, is_admin) values ($1, $2, $3)",
+    [username, hashedPassword, true]
+  );
+
+  await client.end();
+}
+
+const username = process.argv[2];
+const password = process.argv[3];
+
+loadAdminUser(username, password);
