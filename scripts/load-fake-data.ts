7638b529877fb109a834e35d9c7163875942679d
Timilehin Arigbede <harriemannie@gmail.com>
2023-09-08 10:01:33 +0100

    Hash User Passwords

--- scripts/load-fake-data.ts
+++ scripts/load-fake-data.ts
@@ -3,6 +3,7 @@
 import { Client } from "pg";
 import { loadEnvConfig } from "@next/env";
 import { faker } from "@faker-js/faker";
+import bcrypt from "bcrypt";
 
 const projectDir = process.cwd();
 
@@ -25,9 +26,12 @@ async function loadFakeData(numUsers: number = 10) {
     await client.query("begin");
 
     for (let i = 0; i < numUsers; i++) {
+      const saltRounds = 10;
+      const hashedPassword = await bcrypt.hash("123456", saltRounds);
+
       await client.query(
         "insert into public.users (username, password, avatar) values ($1, $2, $3)",
-        [faker.internet.userName(), "password", faker.image.avatar()]
+        [faker.internet.userName(), hashedPassword, faker.image.avatar()]
       );
     }
 
