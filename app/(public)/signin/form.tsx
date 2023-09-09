6f6e8cf271f96b63ed6d7c30f105584103435346
Timilehin Arigbede <harriemannie@gmail.com>
2023-09-09 05:42:23 +0100

    Create Login Form and Page

---
+++ app/(public)/signin/form.tsx
@@ -0,0 +1,65 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
+import React, { FormEvent, useState } from "react";
+
+const Form = () => {
+  const router = useRouter();
+  const [username, setUsername] = useState<undefined | string>("");
+  const [password, setPassword] = useState<undefined | string>("");
+
+  async function handleSubmit(e: FormEvent) {
+    e.preventDefault();
+
+    const res = await fetch("/api/login", {
+      method: "post",
+      body: JSON.stringify({ username, password }),
+    });
+    if (res.ok) {
+      router.push("/feed");
+    } else {
+      alert("log in failed");
+    }
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <h3>Sign In</h3>
+      </div>
+      <div>
+        <hr />
+      </div>
+      <div>
+        <div>
+          <label>Username</label>
+          <input
+            className="text-black"
+            type="text"
+            onChange={(e) => setUsername(e.target.value)}
+            value={username}
+            id="username"
+            placeholder="Username"
+            required
+          />
+        </div>
+        <div>
+          <label>Password</label>
+          <input
+            className="text-black"
+            type="password"
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+            id="password"
+            placeholder="Password"
+            required
+          />
+        </div>
+        <button type="submit">Sign In</button>
+      </div>
+    </form>
+  );
+};
+
+export default Form;
