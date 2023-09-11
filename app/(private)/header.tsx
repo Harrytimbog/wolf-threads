29aacc1664860e3407014001d6f7aa937abaa51e
Timilehin Arigbede <harriemannie@gmail.com>
2023-09-11 22:00:36 +0100

    Header component, User component

--- app/(private)/header.tsx
+++ app/(private)/header.tsx
@@ -2,13 +2,22 @@
 
 import useSWR from "swr";
 import React from "react";
+import User from "../components/user";
 
 const Header = () => {
   const { data, error, isLoading } = useSWR("/api/users/profile");
   if (error) return <div>Failed to load</div>;
   if (isLoading) return <div>Loading...</div>;
-  console.log(data);
-  return <header>{data.data.username}</header>;
+  return (
+    <header className="flex flex-row w-full p-5 bg-slate-800 rounded-lg my-2 justify-between items-center">
+      <div>
+        <h1 className="font-mono text-lg">Wolf Threads</h1>
+      </div>
+      <div>
+        <User user={data.data} href="account" />
+      </div>
+    </header>
+  );
 };
 
 export default Header;
