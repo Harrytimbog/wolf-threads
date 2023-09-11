2f231e0bc1c0ca9eb72096e7af6958d90fe2eefb
Timilehin Arigbede <harriemannie@gmail.com>
2023-09-11 22:23:16 +0100

    Style Navbar and Footer

--- app/(private)/footer.tsx
+++ app/(private)/footer.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Footer = () => {
-  return <footer>Footer</footer>;
+  return <footer>&copy; Wolf Threads {new Date().getFullYear()}</footer>;
 };
 
 export default Footer;
