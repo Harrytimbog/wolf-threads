243e02a2aa47a629b6a81326a95bd4671c28113f
Timilehin Arigbede <harriemannie@gmail.com>
2023-09-11 17:22:13 +0100

    Refactor fetcher and make it available in all private pages | SWR Config

--- app/(private)/header.tsx
+++ app/(private)/header.tsx
@@ -3,22 +3,8 @@
 import useSWR from "swr";
 import React from "react";
 
-const fetcher = async (url: RequestInfo | URL) => {
-  const res = await fetch(url);
-
-  if (!res.ok) {
-    const msg = "An error occurred while fetching the data.";
-    const info = await res.json();
-    const status = res.status;
-    const error = new Error(msg);
-    console.error(info, status);
-    throw error;
-  }
-  return res.json();
-};
-
 const Header = () => {
-  const { data, error, isLoading } = useSWR("/api/users/profile", fetcher);
+  const { data, error, isLoading } = useSWR("/api/users/profile");
   if (error) return <div>Failed to load</div>;
   if (isLoading) return <div>Loading...</div>;
   console.log(data);
