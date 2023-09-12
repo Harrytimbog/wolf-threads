6d857221932594677a06dd2c8a05c8e6ce797a0a
Timilehin Arigbede <harriemannie@gmail.com>
2023-09-12 09:43:40 +0100

    Fix the 500 Error

--- middleware.ts
+++ middleware.ts
@@ -4,24 +4,24 @@ import { NextRequest, NextResponse } from "next/server";
 export async function middleware(request: NextRequest) {
   const pathname = request.nextUrl.pathname;
 
-  const authenticatedAPIRoutes = [pathname.startsWith("/api/users")];
+  const authenticatedAPIRoutes = [
+    pathname.startsWith("/api/users"),
+    pathname.startsWith("/api/posts"),
+  ];
 
   if (authenticatedAPIRoutes.includes(true)) {
     const cookie = request.cookies.get("jwt-token");
 
     if (!cookie || !cookie.value) {
-      return NextResponse.json({ error: "unauthenticated" });
+      return NextResponse.json({ error: "unauthenticated" }, { status: 401 });
     }
 
     try {
       const secret = new TextEncoder().encode(process.env.JWT_SECRET);
       await jwtVerify(cookie.value, secret);
     } catch (error) {
-      console.log(error);
-      return NextResponse.json(
-        { error: "internal server error" },
-        { status: 500 }
-      );
+      // console.log(error);
+      return NextResponse.json({ error: "unauthenticated" }, { status: 401 });
     }
   }
 }
