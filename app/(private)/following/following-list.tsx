e5746288a8b0da5a4699919b1dafacd7bc320eb7
Timilehin Arigbede <harriemannie@gmail.com>
2023-09-13 17:09:49 +0100

    Build UI for Following Page And API End Point

---
+++ app/(private)/following/following-list.tsx
@@ -0,0 +1,25 @@
+import User from "@/app/components/user";
+import useSWR from "swr";
+
+const FollowingList = ({ index }: { index: number }) => {
+  const { data: userData } = useSWR("/api/users/profile");
+  const { data: followerData } = useSWR(
+    () => "/api/users/" + userData.data.id + "/following?page=" + index
+  );
+
+  if (!followerData) return <div>Loading...</div>;
+
+  return (
+    <ul>
+      {followerData.data.map((user: UserI) => {
+        return (
+          <li key={user.id} className="my-5">
+            <User user={user} />
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+
+export default FollowingList;
--- app/(private)/following/page.tsx
+++ app/(private)/following/page.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import FollowingContainer from "./following-container";
 
 const Following = () => {
-  return <div>Following</div>;
+  return (
+    <main>
+      <div>Following</div>
+      <FollowingContainer />
+    </main>
+  );
 };
 
 export default Following;
