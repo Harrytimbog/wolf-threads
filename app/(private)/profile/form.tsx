8708638bd22ebb2bffe9eedbf35843f5ed47c9d7
Timilehin Arigbede <harriemannie@gmail.com>
2023-09-13 13:07:01 +0100

    Create Post Form

---
+++ app/(private)/profile/form.tsx
@@ -0,0 +1,37 @@
+import React, { FormEvent, useState } from "react";
+import { useSWRConfig } from "swr";
+
+const Form = () => {
+  const { mutate } = useSWRConfig();
+  const [post, setPost] = useState("");
+
+  const handleSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+
+    const res = await fetch("/api/posts", {
+      method: "POST",
+      body: JSON.stringify({ content: post }),
+    });
+
+    if (res.ok) {
+      setPost("");
+      
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <textarea
+        className="bg-gray-700 p-2 rounded-lg w-full my-2"
+        placeholder="What are you thinking?"
+        onChange={(e) => setPost(e.target.value)}
+        value={post}
+      />
+      <button type="submit" className="bg-slate-900 p-2 rounded-lg">
+        Post
+      </button>
+    </form>
+  );
+};
+
+export default Form;
--- app/(private)/profile/page.tsx
+++ app/(private)/profile/page.tsx
@@ -1,7 +1,23 @@
+"use client";
+
 import React from "react";
+import useSWR from "swr";
+import Form from "./form";
 
 const Profile = () => {
-  return <div>Profile</div>;
+  const { data, error, isLoading } = useSWR("/api/users/profile");
+
+  if (error) return <div>Failed to load</div>;
+  if (isLoading) return <div>loading...</div>;
+
+  console.log(data);
+
+  return (
+    <main>
+      <h2>Profile</h2>
+      <Form />
+    </main>
+  );
 };
 
 export default Profile;
