9dc85953609e23393926a3030edcccc58711c178
Timilehin Arigbede <harriemannie@gmail.com>
2023-09-13 22:08:28 +0100

    Followers Page UI

--- app/(private)/followers/page.tsx
+++ app/(private)/followers/page.tsx
@@ -1,7 +1,10 @@
+import FollowersContainer from "./followers-container";
+
 const Followers = async () => {
   return (
     <main>
       <h2>Followers</h2>
+      <FollowersContainer />
     </main>
   );
 };
